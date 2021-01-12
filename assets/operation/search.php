e8db1bd623f739bd32bee9a661cf755dfdc840b4
Ahmed Muhammed AbdelHakim <70027345+KaimoKid@users.noreply.github.com>
2021-01-12 22:07:38 +0200

    Search for pages&groups
    -Added Search for groups and pages feature

--- assets/operation/search.php
+++ assets/operation/search.php
@@ -9,9 +9,13 @@
 
                 $index = $_GET['index'];
                 $connect = new connection;
-
+                
                 $results = $connect->conn->query("SELECT full_name,profile_pic,id FROM users WHERE email='$index' OR id='$index'");
-                if (mysqli_num_rows($results) != 0){
+                $pattern = '-1';
+                if(strlen($index) > 1)$pattern = substr($index,1);
+                $groups = $connect->conn->query("SELECT * FROM groups WHERE id='$pattern'");
+                $pages = $connect->conn->query("SELECT * FROM pages WHERE id='$pattern'");
+                if (mysqli_num_rows($results) != 0 && $index[0]!='G' && $index[0]!='P'){
                     for($i = 0; $i < mysqli_num_rows($results);$i++){
                         $result = mysqli_fetch_assoc($results);
                         echo '
@@ -20,22 +24,45 @@
                         <img src="http://localhost/social-media-platform-web/'.$result["id"].'/'.$result["profile_pic"].'"><p>'.$result["full_name"].'</p>
                         </div></a>';
                     }
-                }   
+                }
+                else if(mysqli_num_rows($groups) != 0 && $index[0]=='G'){
+                    for($i = 0; $i < mysqli_num_rows($groups);$i++){
+                        $group = mysqli_fetch_assoc($groups);
+                        echo '
+                        <a href="http://localhost/social-media-platform-web/group.php?group='.$group["id"].'">
+                        <div class="searchUnit">
+                        <img src="http://localhost/social-media-platform-web/assets/img/group_icon.jpg"><p>'.$group["group_name"].'</p>
+                        </div></a>';
+                    }
+                }
+                else if(mysqli_num_rows($pages) != 0 && $index[0]=='P'){
+                    for($i = 0; $i < mysqli_num_rows($pages);$i++){
+                        $page = mysqli_fetch_assoc($pages);
+                        echo '
+                        <a href="http://localhost/social-media-platform-web/page.php?page='.$page["id"].'">
+                        <div class="searchUnit">
+                        <img src="http://localhost/social-media-platform-web/assets/img/page_icon.png"><p>'.$page["page_name"].'</p>
+                        </div></a>';
+                    }
+                }
                 else{
                     $results = $connect->conn->query("SELECT full_name,profile_pic,id FROM users WHERE INSTR(full_name,'$index')>0");
+                    $groups = $connect->conn->query("SELECT * FROM groups WHERE INSTR(group_name,'$index')>0");
+                    $pages = $connect->conn->query("SELECT * FROM pages WHERE INSTR(page_name,'$index')>0");
+
                     if (strlen($index) < 1) {
                         echo '
                         <div class="searchUnit">
                         <samp>Search Results will be shown here.</samp>
                         </div>';
                     }
-                    else if (mysqli_num_rows($results) == 0) {
+                   else if (mysqli_num_rows($results) == 0 && mysqli_num_rows($groups) == 0 && mysqli_num_rows($pages) == 0) {
                         echo '
                         <div class="searchUnit">
                         <samp>No Matching Results.</samp>
                         </div>';
-                    }
-                    else if (mysqli_num_rows($results) == 1) {
+                    } 
+                    else if (mysqli_num_rows($results) == 1 && mysqli_num_rows($groups) == 0 && mysqli_num_rows($pages) == 0) {
                         $result = mysqli_fetch_assoc($results);
                         if($result['id']==$_SESSION['user']->get_id())
                         echo '
@@ -49,15 +76,33 @@
                         <img src="http://localhost/social-media-platform-web/'.$result["id"].'/'.$result["profile_pic"].'"><p>'.$result["full_name"].'</p>
                         </div></a>';
                     }
-                    else
-                    for($i = 0; $i < mysqli_num_rows($results);$i++){
-                        $result = mysqli_fetch_assoc($results);
-                        if($result['id']!=$_SESSION['user']->get_id())
-                        echo '
-                            <a href="http://localhost/social-media-platform-web/'.$result["id"].'">
+
+                    else{
+                        for($i = 0; $i < mysqli_num_rows($results);$i++){
+                            $result = mysqli_fetch_assoc($results);
+                            if($result['id']==$_SESSION['user']->get_id()) continue;
+                            echo '
+                                <a href="http://localhost/social-media-platform-web/'.$result["id"].'">
+                                <div class="searchUnit">
+                                <img src="http://localhost/social-media-platform-web/'.$result["id"].'/'.$result["profile_pic"].'"><p>'.$result["full_name"].'</p>
+                                </div></a>';
+                        }
+                        for($i = 0; $i < mysqli_num_rows($groups);$i++){
+                            $group = mysqli_fetch_assoc($groups);
+                            echo '
+                            <a href="http://localhost/social-media-platform-web/group.php?group='.$group["id"].'">
                             <div class="searchUnit">
-                            <img src="http://localhost/social-media-platform-web/'.$result["id"].'/'.$result["profile_pic"].'"><p>'.$result["full_name"].'</p>
+                            <img src="http://localhost/social-media-platform-web/assets/img/group_icon.jpg"><p>'.$group["group_name"].'</p>
                             </div></a>';
+                        }
+                        for($i = 0; $i < mysqli_num_rows($pages);$i++){
+                            $page = mysqli_fetch_assoc($pages);
+                            echo '
+                            <a href="http://localhost/social-media-platform-web/page.php?page='.$page["id"].'">
+                            <div class="searchUnit">
+                            <img src="http://localhost/social-media-platform-web/assets/img/page_icon.png"><p>'.$page["page_name"].'</p>
+                            </div></a>';
+                        }
                     }
                 }
             }
